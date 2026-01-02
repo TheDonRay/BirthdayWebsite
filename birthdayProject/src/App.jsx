2c9859c0f515e669c739caf03dd8e249f1fbdf28
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-01 21:13:13 -0500

    website for my bestfriend

---
+++ birthdayProject/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css' 
+import Homepage from './components/homepage.jsx'
+
+function App() {
+  return (
+    <> 
+    <Homepage/>
+    </> 
+  )
+}
+
+export default App;
---
+++ birthdayProject/src/components/homepage.jsx
@@ -0,0 +1,6 @@
+export default function Homepage() { 
+    return( 
+        <h1>Welcome to homepage</h1>
+    )
+}; 
+
